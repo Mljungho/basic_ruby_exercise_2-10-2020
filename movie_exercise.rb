9895eeb015a888f52f6cbad2e48ccf45da7d432f
Mljungho <mathias.ljungholm@gmail.com>
2021-11-02 22:35:15 +0100

    Lisingt my name & entire hashes in array. Need to figure out how to extraxt objects nested in a hash, that is nested in an array

---
+++ movie_exercise.rb
@@ -0,0 +1,17 @@
+name = "Mathias"
+surname = "Ljungholm"
+
+puts name + " " + surname
+
+
+aBeautifullMind = {title: "A beautifull mind", release: 2001}
+fightClub = {title: "Fight Club", release: 1999}
+kungPow = {title: "Kung Pow", release: 2002}
+lobster = {title: "Lobster", release: 2015}
+winterSleep = {title: "Winter Sleep", release: 2014}
+
+imdb = [aBeautifullMind, fightClub, kungPow, lobster, winterSleep]
+
+imdb.each do |release|
+    puts "#{release}"
+end
