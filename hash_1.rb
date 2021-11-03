bda114a29d85895cf3ef7e7181235d0e08b9bbe1
Mljungho <mathias.ljungholm@gmail.com>
2021-11-03 18:26:54 +0100

    my own version of previus exercise

---
+++ hash_1.rb
@@ -0,0 +1,19 @@
+#Given a hash of family members, with keys as the title and an array of names as the 
+#values, use Ruby's built-in select method to gather only immediate family members' names into a new array.
+
+family = {  uncles: ['bob', 'joe', 'steve'],
+            sisters: ['jane', 'jill', 'beth'],
+            brothers: ['frank', 'rob', 'david'],
+            aunts: ['mary', 'sally', 'susan'],
+
+}
+
+
+close_family = family[:sisters], family[:brothers]
+#colecting the keys :sisters, :brothers, from the family hash in a new object = close_family
+core_family = []
+#creates an empty array
+core_family.push close_family
+#pushes the content from Close family to Core family array.
+puts core_family
+#displays content of core_family
