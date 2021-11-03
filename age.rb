ebfa237be8dbc49acd59a631a61362996f482801
Mljungho <mathias.ljungholm@gmail.com>
2021-11-03 09:45:45 +0100

    Added age and name ruby exercise. Practiced to_i and to_f in age to treat the age as integer / float

---
+++ age.rb
@@ -0,0 +1,8 @@
+puts "How old are you?"
+
+age = gets
+
+puts "in 10 years you will be #{age.to_f + 10} years old"
+puts "in 20 years you will be #{age.to_i + 20} years old"
+puts "in 30 years you will be #{age.to_i + 30} years old"
+puts "in 40 years you will be #{age.to_i + 40} years old"
---
+++ name.rb
@@ -0,0 +1,10 @@
+puts "what's your name?"
+
+name = gets
+
+puts "Welcome to Göteborg, #{name}!"
+
+10.times do
+    puts "#{name}"
+
+end
