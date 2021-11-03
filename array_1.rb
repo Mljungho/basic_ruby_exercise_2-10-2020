554c5fd39e32464ae086db47595ba61f7eebac4b
Mljungho <mathias.ljungholm@gmail.com>
2021-11-03 15:22:43 +0100

    finallized two exercises

---
+++ array_1.rb
@@ -0,0 +1,8 @@
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+puts arr.index(5) #it returns the first index of the number 5 inside the array, which is 3. (second is 6)
+
+#arr.index[5] #it returns an error. You can't use square brackets with the .index method
+
+puts arr[5] #it returns 8, the fifth object of the array.
---
+++ array_3.rb
@@ -0,0 +1,6 @@
+puts string = "Welcome to America!" #string is a string.
+puts a = string[6]  # a specifies an array value on the string-value. in this case nr7 key of "Welcome to america" = e
+puts b = string[11] # b = same as above but in this case key #12 [11] = A
+puts c = string[19] # c does the same thing, but the string doesn't span as far as the C array targets.
+
+puts string.length # just to prove argument C, this shows string length is 19 characters long. C ask's for the 20:th character witch is nonegsistent.
